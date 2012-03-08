2a408798ca7fe6696b6559261138a12a34c41903
BB9z <BB9z@live.com>
2012-03-08 14:23:47 +0800

    F UIColor (RFKit) 声明

--- extension/UIColor+RFKit.h
+++ extension/UIColor+RFKit.h
@@ -6,7 +6,7 @@
 @interface UIColor (RFKit)
 
 - (UIColor *)initWithRGBHex:(NSInteger)hexValue alpha:(CGFloat)alpha;
-+ (UIColor*)colorWithRGBHex:(NSInteger)hexValue alpha:(CGFloat)alphaValue;
++ (UIColor*)colorWithRGBHex:(NSInteger)hexValue alpha:(CGFloat)alpha;
 + (UIColor*)colorWithRGBHex:(NSInteger)hexValue;
 
 @end
