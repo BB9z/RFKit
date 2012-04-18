c47375ca3b02b30053b92555ec023cc91feeecac
BB9z <BB9z@live.com>
2012-04-18 10:46:58 +0800

    NSURL+RFKit 	+ queryDictionary

---
+++ extension/NSURL+RFKit.h
@@ -0,0 +1,17 @@
+/*!
+	RFKit
+	NSURL extension
+ 
+	ver 0.0.1
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (RFKit)
+
+/**	Cover query string into NSDictionary
+ */
+- (NSDictionary *)queryDictionary;
+
+
+@end
---
+++ extension/NSURL+RFKit.m
@@ -0,0 +1,26 @@
+
+#import "NSURL+RFKit.h"
+
+@implementation NSURL (RFKit)
+
+
+- (NSDictionary *)queryDictionary {
+	NSString * queryString = [self query];
+	if (!queryString) return nil;
+	
+	NSMutableDictionary *queryDictionary = [NSMutableDictionary dictionary];
+	
+	NSCharacterSet *charSetAmpersand = [NSCharacterSet characterSetWithCharactersInString:@"&"];
+	NSCharacterSet *charSetEqualsSign = [NSCharacterSet characterSetWithCharactersInString:@"="];
+	for (NSString *fieldValuePair in [queryString componentsSeparatedByCharactersInSet:charSetAmpersand]) {
+		NSArray *fieldValueArray = [fieldValuePair componentsSeparatedByCharactersInSet:charSetEqualsSign];
+		if (fieldValueArray.count == 2) {
+			NSString *filed = [fieldValueArray objectAtIndex:0];
+			NSString *value = [fieldValueArray objectAtIndex:1];
+			[queryDictionary setObject:[value stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding] forKey:filed];
+		}
+	}
+	_douto(queryDictionary)
+	return queryDictionary;
+}
+@end
