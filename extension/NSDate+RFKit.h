6c4ba9a46f69e27e62e77358261239cc26beb9db
BB9z <bb9z@live.com>
2012-06-09 20:48:15 +0800

    New NSDate(RFKit)

---
+++ extension/NSDate+RFKit.h
@@ -0,0 +1,13 @@
+/*!
+    RFKit
+    NSDate
+ 
+    ver -.-.-
+ */
+
+#import "RFKit.h"
+
+@interface NSDate (RFKit)
+
+- (BOOL)isSameDayWithDate:(NSDate *)date;
+@end
---
+++ extension/NSDate+RFKit.m
@@ -0,0 +1,11 @@
+
+#import "NSDate+RFKit.h"
+
+@implementation NSDate (RFKit)
+
+- (BOOL)isSameDayWithDate:(NSDate *)date {
+    NSDateComponents *target = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:date];
+    NSDateComponents *source = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:self];
+    return [target isEqual:source];
+}
+@end
