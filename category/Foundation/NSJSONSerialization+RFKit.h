5c4a063880265e538d1e24b5c9151bce4376db9f
BB9z <bb9z@me.com>
2015-03-10 21:03:18 +0800

    NSJSONSerialization+RFKit, methods support nil input now.

--- category/Foundation/NSJSONSerialization+RFKit.h
+++ category/Foundation/NSJSONSerialization+RFKit.h
@@ -15,9 +15,9 @@
 
 /** Generate a string containing JSON data from a Foundation object.
  
- If the object will not produce valid JSON then an exception will be thrown. Setting the NSJSONWritingPrettyPrinted option will generate JSON with whitespace designed to make the output more readable. If that option is not set, the most compact possible JSON will be generated. If an error occurs, the error parameter will be set and the return value will be nil.
+ If the object will not produce valid JSON and not nil then an exception will be thrown. Setting the NSJSONWritingPrettyPrinted option will generate JSON with whitespace designed to make the output more readable. If that option is not set, the most compact possible JSON will be generated. If an error occurs, the error parameter will be set and the return value will be nil.
  
- @param obj     The object from which to generate JSON data.
+ @param obj     The object from which to generate JSON data. May be `nil`.
  @param opt     Options for creating the JSON data.
  @param error   If an error occurs, upon return contains an NSError object that describes the problem.
 
@@ -30,7 +30,7 @@
  
  This method use [NString dataUsingEncoding:allowLossyConversion:] cover the string to NSData, then use [NSJSONSerialization JSONObjectWithData:options:error:] cover the data object to Foundation object.
   
- @param string  A string containing JSON data.
+ @param string  A string containing JSON data. May be `nil`.
  @param encoding A string encoding.
  @param lossy   If YES, then allows characters to be removed or altered in conversion.
  @param opt     Options for reading the JSON data and creating the Foundation objects.
@@ -42,7 +42,7 @@
 
 /** Returns a Foundation object from given JSON string.
  
- @param string  A string containing JSON data.
+ @param string  A string containing JSON data. May be `nil`.
  
  @return A data object containing JSON string.
  
--- category/Foundation/NSJSONSerialization+RFKit.m
+++ category/Foundation/NSJSONSerialization+RFKit.m
@@ -5,6 +5,7 @@
 @implementation NSJSONSerialization (RFKit)
 
 + (NSString *)stringWithJSONObject:(id)obj options:(NSJSONWritingOptions)opt error:(NSError *__autoreleasing *)error {
+    if (!obj) return nil;
     NSData *d = [NSJSONSerialization dataWithJSONObject:obj options:opt error:error];
     return RF_AUTORELEASE([[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding]);
 }
