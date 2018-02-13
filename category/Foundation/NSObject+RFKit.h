acd70f5847ec509e0e8686c6706b7689aa530459
BB9z <bb9z@me.com>
2018-02-13 09:53:52 +0800

    NSObject+RFKit, black history, omg.

--- category/Foundation/NSObject+RFKit.h
+++ category/Foundation/NSObject+RFKit.h
@@ -2,7 +2,7 @@
     NSObject extension
     RFKit
 
-    Copyright (c) 2012-2016 BB9z
+    Copyright (c) 2012-2016, 2018 BB9z
     https://github.com/BB9z/RFKit
 
     The MIT License (MIT)
@@ -13,8 +13,7 @@
 
 @interface NSObject (RFKit)
 
-+ (void)defaultFill:(id _Nullable)test with:(id _Nullable)value;
-- (id _Nullable)get:(id _Nullable)test defaults:(id _Nullable)value;
+- (id _Nullable)get:(id _Nullable)test defaults:(id _Nullable)value DEPRECATED_ATTRIBUTE;
 
 - (NSArray<id> *_Nonnull)objectsForIndexArray:(NSArray<id> *_Nullable)indexsArray;
 - (NSArray<id> *_Nullable)objectsForDictKeyArray:(NSArray<NSString *> *_Nonnull)keyArray;
--- category/Foundation/NSObject+RFKit.m
+++ category/Foundation/NSObject+RFKit.m
@@ -4,12 +4,6 @@
 
 @implementation NSObject (RFKit)
 
-+ (void)defaultFill:(id)test with:(id)value {
-	if (test == nil || test == [NSNull null]) {
-        test = value;
-	}
-}
-
 - (id)get:(id)test defaults:(id)value {
 	if (test == nil || test == [NSNull null]) {
 		return value;
