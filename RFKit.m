93452238741413737fe75a1c5a5ef8813ac750bf
BB9z <bb9z@me.com>
2018-02-25 20:43:00 +0800

    Use __has_feature to qualify the xcode version supported.

--- RFKit.m
+++ RFKit.m
@@ -3,9 +3,14 @@
 
 double RFKitVersionNumber = 1007000.0;
 
-// Xcode 7: Generics
-#ifndef __IPHONE_9_0
-#   error "RFKit require Xcode 7 or later to build."
+// Xcode 7: 6294649 Lightweight generics
+#if !__has_feature(objc_generics)
+#   error "RFKit requires generics which is provided on Xcode 7 or later."
+#endif
+
+// Xcode 7: 19589424
+#if !__has_feature(objc_kindof)
+#   error "RFKit requires __kindof type which is provided on Xcode 7 or later."
 #endif
 
 #if !__has_feature(objc_arc)
