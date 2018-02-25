a82c04812cb86415f7df8c18ffaf0256a076a27b
BB9z <bb9z@me.com>
2018-02-25 21:41:26 +0800

    New RF_AS_NONNULL() to converts nullable expression to nonnull.

--- RFFeatureSupport.h
+++ RFFeatureSupport.h
@@ -60,6 +60,42 @@
 #  define RF_WARN_UNUSED_RESULT
 #endif
 
+// We purposefully don't have a matching @implementation.
+// We don't want +asNonnull to ever actually be called
+// because that will add a lot of overhead to every RBBNotNil
+// and we want RBBNotNil to be very cheap.
+// If there is no @implementation, then if the +asNonnull is
+// actually called, we'll get a linker error complaining about
+// the lack of @implementation.
+@interface RF_NONNULL <__covariant Type>
+// This as a class method so you don't need to
+// declare an unused lvalue just for a __typeof
++ (Type _Nonnull)asNonnull;
+@end
+
+/**
+ @define RF_AS_NONNULL(V)
+ 
+ Converts an Objective-C object expression from _Nullable to _Nonnull.
+ 
+ Crashes if it receives a nil! We must crash or else we'll receive static analyzer warnings when archiving. I think in Release mode, the compiler ignores the _Nonnull cast.
+ 
+ @param V a _Nullable Objective-C object expression
+ 
+ @ref https://gist.github.com/robb/d55b72d62d32deaee5fa
+ */
+#if DEBUG
+#define RF_AS_NONNULL(V) \
+({\
+    __typeof__(V) __nullableV = V;\
+    NSCAssert(__nullableV, @"Expected '%@' not to be nil.", @#V);\
+    (__typeof([RF_NONNULL<__typeof(V)> asNonnull]))__nullableV;\
+})
+#else
+#define RF_AS_NONNULL(V) \
+    (__typeof([RF_NONNULL<__typeof(V)> asNonnull]))V
+#endif
+
 
 #pragma mark - SDK backward compatibility
 
