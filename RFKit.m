5d7e1be57871f2e9198462c451a69fcf00d7bb91
BB9z <bb9z@me.com>
2018-02-25 15:53:28 +0800

    typo.

--- RFKit.m
+++ RFKit.m
@@ -3,10 +3,11 @@
 
 double RFKitVersionNumber = 1007000.0;
 
+// Xcode 7: Generics
 #ifndef __IPHONE_9_0
-#   error "RFKit require iOS SDK 9.0 or later to build."
+#   error "RFKit require Xcode 7 or later to build."
 #endif
 
 #if !__has_feature(objc_arc)
-#   error "You should build RFKit with ARC."
+#   error "You must build RFKit with ARC."
 #endif
