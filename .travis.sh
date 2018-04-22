ffc1b4b8ca714c7eee2b6b69803c8842e050cf41
BB9z <bb9z@me.com>
2018-04-22 20:28:29 +0800

    #ci script options.
    [skip lint]

--- .travis.sh
+++ .travis.sh
@@ -3,13 +3,38 @@
 set -eo pipefail
 # set -euxo pipefail
 
-echo $TRAVIS_COMMIT
 echo $TRAVIS_COMMIT_MESSAGE
-echo $TRAVIS_BRANCH
 echo "RFCI_TASK = $RFCI_TASK"
 
+# Run test
+# $1 scheme
+# $2 destination
+XC_Test() {
+    xcodebuild test -enableCodeCoverage YES -workspace Test/RFKit.xcworkspace -scheme "$1" -destination "$2" ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+}
+
+# Run macOS test
+XC_TestMac() {
+    xcodebuild test -enableCodeCoverage YES -workspace Test/RFKit.xcworkspace -scheme "Test-macOS" GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+}
+
+# Run watchOS test
+XC_TestWatch() {
+    xcodebuild build -workspace Test/RFKit.xcworkspace -scheme Target-watchOS ONLY_ACTIVE_ARCH=NO | xcpretty
+}
+
 if [ "$RFCI_TASK" = "POD_LINT" ]; then
-    pod lib lint --allow-warnings
+    if [ $TRAVIS_COMMIT_MESSAGE = *"[skip lint]"* ]; then
+        echo "Skip pod lint"
+        exit 0
+    fi
+
+    echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+    if [ "$TRAVIS_BRANCH" = "develop"]; then
+        pod lib lint --allow-warnings
+    else
+        pod lib lint
+    fi
 elif [ "$RFCI_TASK" = "Xcode9" ]; then
     echo "Test for macOS and watchOS."
     XC_TestMac
@@ -34,20 +59,3 @@ elif [ "$RFCI_TASK" = "Xcode8" ]; then
     XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone 6,OS=10.2"
     XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV 1080p,OS=10.2"
 fi
-
-# Run test
-# $1 scheme
-# $2 destination
-XC_Test() {
-    xcodebuild test -enableCodeCoverage YES -workspace Test/RFKit.xcworkspace -scheme "$1" -destination "$2" ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
-}
-
-# Run macOS test
-XC_TestMac() {
-    xcodebuild test -enableCodeCoverage YES -workspace Test/RFKit.xcworkspace -scheme "Test-macOS" GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
-}
-
-# Run watchOS test
-XC_TestWatch() {
-    xcodebuild build -workspace Test/RFKit.xcworkspace -scheme Target-watchOS ONLY_ACTIVE_ARCH=NO | xcpretty
-}
