035058e46cfb03e1b149936724b3ffd51da4cfe6
BB9z <bb9z@me.com>
2018-04-24 09:15:54 +0800

    #ci update script.

--- .travis.sh
+++ .travis.sh
@@ -24,44 +24,64 @@ XC_TestWatch() {
     xcodebuild build -workspace "$RFWorkspace" -scheme Target-watchOS ONLY_ACTIVE_ARCH=NO | xcpretty
 }
 
-if [ "$RFCI_TASK" = "POD_LINT" ]; then
-    if [[ "$TRAVIS_COMMIT_MESSAGE" = *"[skip lint]"* ]]; then
-        echo "Skip pod lint"
-    else
-        echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
-        gem install cocoapods --no-rdoc --no-ri --no-document --quiet
-        if [ "$TRAVIS_BRANCH" = "develop" ]; then
-            pod lib lint --allow-warnings
+STAGE_MAIN() {
+    if [ "$RFCI_TASK" = "POD_LINT" ]; then
+        if [[ "$TRAVIS_COMMIT_MESSAGE" = *"[skip lint]"* ]]; then
+            echo "Skip pod lint"
         else
-            pod lib lint
+            echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+            gem install cocoapods --no-rdoc --no-ri --no-document --quiet
+            if [ "$TRAVIS_BRANCH" = "develop" ]; then
+                pod lib lint --allow-warnings
+            else
+                pod lib lint
+            fi
         fi
+
+    elif [ "$RFCI_TASK" = "Xcode9" ]; then
+        pod install --project-directory=Test
+
+        echo "Test for macOS and watchOS."
+        XC_TestMac
+        XC_TestWatch
+
+        echo "Test on lastest device and OS."
+        XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone X,OS=11.3"
+        XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV 4K,OS=11.3"
+
+        echo "Test on old device and OS".
+        XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone 5,OS=9.0"
+        XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV,OS=9.0"
+
+        echo "Test Swift"
+        XC_Test "Test-Swift" "platform=iOS Simulator,name=iPhone X,OS=11.3"
+
+    elif [ "$RFCI_TASK" = "Xcode8" ]; then
+        pod install --project-directory=Test
+
+        echo "Test for macOS and watchOS."
+        XC_TestMac
+        XC_TestWatch
+        
+        XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone 6,OS=10.2"
+        XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV 1080p,OS=10.2"
     fi
+}
 
-elif [ "$RFCI_TASK" = "Xcode9" ]; then
-    pod install --project-directory=Test
+STAGE_SUCCESS() {
+    if [ "$RFCI_TASK" = "Xcode9" ]; then
+        curl -s https://codecov.io/bash | bash -s
+    fi
+}
 
-    echo "Test for macOS and watchOS."
-    XC_TestMac
-    XC_TestWatch
+STAGE_FAILURE() {
+    if [[ "$RFCI_TASK" == Xcode* ]]; then
+        cat -n ~/Library/Logs/DiagnosticReports/xctest*.crash
+    fi
+}
 
-    echo "Test on lastest device and OS."
-    XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone X,OS=11.3"
-    XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV 4K,OS=11.3"
-
-    echo "Test on old device and OS".
-    XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone 5,OS=9.0"
-    XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV,OS=9.0"
-
-    echo "Test Swift"
-    XC_Test "Test-Swift" "platform=iOS Simulator,name=iPhone X,OS=11.3"
-
-elif [ "$RFCI_TASK" = "Xcode8" ]; then
-    pod install --project-directory=Test
-
-    echo "Test for macOS and watchOS."
-    XC_TestMac
-    XC_TestWatch
-    
-    XC_Test "Test-iOS"   "platform=iOS Simulator,name=iPhone 6,OS=10.2"
-    XC_Test "Test-tvOS"  "platform=tvOS Simulator,name=Apple TV 1080p,OS=10.2"
+if [ -z "$RFSTAGE" ]; then
+    echo "RFSTAGE is undefined, do nothing."
+else
+    "STAGE_$RFSTAGE"
 fi
