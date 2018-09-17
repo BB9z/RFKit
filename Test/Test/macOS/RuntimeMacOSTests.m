2722574b45517faed6a1daafea9ec78208aaa3d0
BB9z <bb9z@me.com>
2018-09-17 22:51:29 +0800

    #test Xcode 8 fix.

--- Test/Test/macOS/RuntimeMacOSTests.m
+++ Test/Test/macOS/RuntimeMacOSTests.m
@@ -18,14 +18,12 @@
 - (void)testUITypesBridge {
     UIEdgeInsets inset = UIEdgeInsetsMake(1, 2, 3, 4);
     NSEdgeInsets inset2 = NSEdgeInsetsMake(1, 2, 3, 4);
-    if (@available(macOS 10.10, *)) {
-        XCTAssert(UIEdgeInsetsEqualToEdgeInsets(inset, inset2));
-        
-        // Strange, use NSEdgeInsetsZero case build fails. Xcode 10
+    XCTAssert(UIEdgeInsetsEqualToEdgeInsets(inset, inset2));
+    
+    // Strange, use NSEdgeInsetsZero case build fails. Xcode 10
 //        NSEdgeInsets inset4 = NSEdgeInsetsZero;
 //        UIEdgeInsets inset3 = UIEdgeInsetsZero;
 //        XCTAssertFalse(UIEdgeInsetsEqualToEdgeInsets(inset3, inset2));
-    }
     XCTAssert(UIView.class == NSView.class);
     XCTAssert(UIButton.class == NSButton.class);
     XCTAssert(UIImage.class == NSImage.class);
